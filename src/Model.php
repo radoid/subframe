c36551748c7439637485abf74d81dea77b04cc21
Ante Radman <ante@radoid.com>
2016-12-20 10:27:23 +0100

    `Model` class support for SQLite "UPSERT"

--- src/Model.php
+++ src/Model.php
@@ -176,8 +176,13 @@ class Model {
 	 * @return int The number of rows affected
 	 */
 	public function upsert(): int {
-		$sql = "INSERT INTO ".static::TABLE."(".$this->keys().") VALUES (".$this->values().")
-				ON DUPLICATE KEY UPDATE $this";
+		$driver = self::$pdo->getAttribute(PDO::ATTR_DRIVER_NAME);
+		if ($driver == 'sqlite')
+			$sql = "INSERT INTO ".static::TABLE."(".$this->keys().") VALUES (".$this->values().")
+					ON CONFLICT DO UPDATE SET $this";
+		else
+			$sql = "INSERT INTO ".static::TABLE."(".$this->keys().") VALUES (".$this->values().")
+					ON DUPLICATE KEY UPDATE $this";
 		return self::$pdo->exec($sql);
 	}
 
