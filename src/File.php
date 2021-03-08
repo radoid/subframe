dfe7e1b4f95c8769dd4af6021420ca17d6bcd99c
Ante Radman <ante@radoid.com>
2021-03-08 18:33:33 +0100

    Introduced File::uniqueUri()

--- src/File.php
+++ src/File.php
@@ -8,27 +8,45 @@ namespace Subframe;
 class File {
 
 	/**
-	 * Ensures a file path is unique, modifying it if needed, creating directories along the path if needed
-	 * @param string $destination The full path to the file
-	 * @param bool $mkdir Should directories be created if they don't already exist
+	 * Ensures a file path is unique, by appending a numeric suffix if needed.
+	 * Optionally, creates directories along the given path, if needed.
+	 * @param string $filepath The full desired path to the file
+	 * @param int $mkdirMode Optional permissions for the directories, should they be created (if non-zero)
 	 * @return string
 	 */
-	static public function unique($destination, $mkdir = true) {
-		$parts = pathinfo($destination);
-		if ($mkdir)
-			@mkdir($parts['dirname'], 0777, true)
-				and @chmod($parts['dirname'], 0777);
-		$parts['basename'] = preg_replace('/\..*/', '', $parts['basename']);
-		$parts['basename'] = self::safe($parts['basename']);
-		$parts['extension'] = (@$parts['extension'] ? self::safe($parts['extension']) : '');
-		$i = "";
-		if (preg_match('~^(.*)(-\d+)$~', $parts['basename'], $matches)) {
-			$parts['basename'] = $matches[1];
-			$i = $matches[2];
-		}
-		while (file_exists("$parts[dirname]/$parts[basename]$i.$parts[extension]"))
-			$i--;
-		return "$parts[dirname]/$parts[basename]$i.$parts[extension]";
+	static public function unique($filepath, $mkdirMode = 0777) {
+		$parts = pathinfo($filepath);
+		$dir = $parts['dirname'];
+		$base = $parts['filename'];
+		$ext = $parts['extension'] ? ".$parts[extension]" : '';
+		if ($mkdirMode)
+			@mkdir($dir, $mkdirMode, true)
+				and @chmod($dir, $mkdirMode);
+		for ($i = ''; file_exists("$dir/$base$i$ext"); $i--);
+
+		return "$dir/$base$i$ext";
+	}
+
+	/**
+	 * Finds a unique file path, relative to a base directory, by appending a numeric suffix when needed..
+	 * Optionally, creates directories along the given path, if needed.
+	 * @param string $root The base directory
+	 * @param string $filepath The desired path to the file, relative to the base directory
+	 * @param int $mkdirMode Optional permissions for the directories, should they be created (if non-zero)
+	 * @return string
+	 */
+	static public function uniqueUri(string $root, string $uri, $mkdirMode = 0777) {
+		$root = rtrim($root, '/');
+		$parts = pathinfo($uri);
+		$base = "$parts[dirname]/$parts[filename]";
+		$ext = $parts['extension'] ? ".$parts[extension]" : '';
+		$dir = $root.'/'.ltrim($parts['dirname'], '/');
+		if ($mkdirMode)
+			@mkdir($dir, $mkdirMode, true)
+				and @chmod($dir, $mkdirMode);
+		for ($i = ''; file_exists($root.'/'.ltrim("$base$i$ext", '/')); $i--);
+
+		return "$base$i$ext";
 	}
 
 	/**
@@ -37,10 +55,10 @@ class File {
 	 * @return string
 	 */
 	static public function safe($name) {
-		$name = str_replace(["\xC4\x8C", "\xC4\x86", "\xC5\xA0", "\xC4\x90", "\xC5\xBD"], ["c", "c", "s", "dj", "z"], $name);
-		$name = str_replace(["\xC4\x8D", "\xC4\x87", "\xC5\xA1", "\xC4\x91", "\xC5\xBE"], ["c", "c", "s", "dj", "z"], $name);
-		$name = str_replace(" ", "-", trim(preg_replace('/[^a-z0-9]+/', ' ', strtolower($name))));
-		return (strlen($name) ? $name : "1");
+		$name = trim(preg_replace('/[^a-z0-9]+/', '-', strtolower($name)), '-');
+		$name = strlen($name) ? $name : '1';
+
+		return $name;
 	}
 
 }
