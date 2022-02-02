eefa8397898a181bf5869a5e3244265d0f937cbc
Ante Radman <ante@radoid.com>
2022-02-02 19:37:30 +0100

    Router and RouterMiddleware classes refactored

--- src/Router.php
+++ src/Router.php
@@ -7,69 +7,55 @@ namespace Subframe;
  */
 class Router {
 
-	/** @var Request */
-	private $request;
+	/**
+	 * The handled request
+	 * @var Request
+	 */
+	protected $request;
 
-	/** @var string */
-	private $method;
+	/**
+	 * The method of the handled request
+	 * @var string
+	 */
+	protected $method;
 
-	/** @var string */
-	private $uri;
-
-	/** @var Cache|null */
-	private $cache;
+	/**
+	 * The URI of the handled request
+	 * @var string
+	 */
+	protected $uri;
 
 
 	/**
 	 * Creates a router for the given request parameters or for the current request
-	 * @param string|null $method HTTP method/verb
-	 * @param string|null $uri requested URI; if not specified, it's resolved automatically — from REQUEST_URI, but relative to the script's directory (to also take care of small websites that reside in a subdirectory)
-	 * @param Cache|null $cache
+	 * @param Request|null $request
 	 */
-	public function __construct(?Request $request = null, ?Cache $cache = null) {
+	public function __construct(?Request $request = null) {
 		$this->request = $request ?? Request::fromRelativeRequestUri();
 		$this->method = $this->request->getMethod();
 		$this->uri = trim(strtok($this->request->getUri(), '?'), '/');
-		$this->cache = $cache;
 	}
 
 	/**
 	 * Creates a router for the request from the REQUEST_URI constant
-	 * @param Cache|null $cache
 	 * @return Router
 	 */
-	public static function fromRequestUri(?Cache $cache = null): self {
-		return new self(Request::fromRequestUri(), $cache);
+	public static function fromRequestUri(): self {
+		return new self(Request::fromRequestUri());
 	}
 
 	/**
 	 * Creates a router for the request from the PATH_INFO constant
-	 * @param Cache|null $cache
 	 * @return Router
 	 */
-	public static function fromPathInfo(?Cache $cache = null): self {
-		return new self(Request::fromPathInfo(), $cache);
+	public static function fromPathInfo(): self {
+		return new self(Request::fromPathInfo());
 	}
 
 	/**
 	 * Tries to dispatch the request within a namespace
 	 * @param string $namespace The namespace; the root namespace if empty
 	 * @param array $classArgs Optional arguments to the found class' constructor
-	 * @param Cache|null $cache Optional cache if caching is desired
-	 */
-	public function routeInNamespace(string $namespace, array $classArgs = []) {
-		if (($route = $this->findRouteInNamespace($namespace))) {
-			[$class, $action, $args] = $route;
-			$instance = new $class($this->request, ...$classArgs);
-			self::executeCallable([$instance, $action], $args, $this->cache);
-		}
-	}
-
-	/**
-	 * Tries to dispatch the request within a namespace
-	 * @param string $namespace The namespace; the root namespace if empty
-	 * @param array $classArgs Optional arguments to the found class' constructor
-	 * @param Cache|null $cache Optional cache if caching is desired
 	 */
 	public function captureRouteInNamespace(string $namespace, array $classArgs = []): ?Response {
 		if (($route = $this->findRouteInNamespace($namespace))) {
@@ -83,31 +69,7 @@ class Router {
 	}
 
 	/**
-	 * Defines a route: checks if the request is compatible with the given URI, and routes the request to the given callable if it is
-	 * @param string $method The HTTP request method
-	 * @param string $uri The URI for the route
-	 * @param mixed $callable A closure or [Controller, action] combination
-	 * @param array $classArgs Optional arguments to the found class' constructor
-	 * @return Router
-	 */
-	public function route(string $method, string $uri, $callable, array $classArgs = []): self {
-		$uri = trim($uri, '/');
-
-		if ($method == $this->method && preg_match("~^$uri$~", $this->uri, $matches)) {
-			if (is_string($callable) && strpos($callable, '@') !== false)
-				$callable = explode('@', $callable);
-			if (is_array($callable) && is_string($callable[0]))
-				$callable[0] = new $callable[0]($this->request, ...$classArgs);
-			$args = array_slice($matches, 1);
-
-			self::executeCallable($callable, $args, $cache ?? $this->cache);
-		}
-
-		return $this;
-	}
-
-	/**
-	 * Defines a route: checks if the request is compatible with the given URI, and routes the request to the given callable if it is
+	 * Tries to match a route to the request, and returns given callable's response if successful
 	 * @param string $method The HTTP request method
 	 * @param string $uri The URI for the route
 	 * @param mixed $callable A closure or [Controller, action] combination
@@ -132,6 +94,7 @@ class Router {
 	}
 
 	/**
+	 * Tries to match a route to the request, and returns a view as a response if successful
 	 * @return ?Response
 	 */
 	public function captureViewRoute(string $uri, string $filename, array $data = []): ?Response {
@@ -146,75 +109,27 @@ class Router {
 	}
 
 	/**
-	 * Defines a GET route, using route() method
-	 * @param string $uri The URI for the route
-	 * @param mixed $callable A closure or [Controller, action] combination
-	 * @param array $classArgs Optional arguments to the found class' constructor
-	 * @return Router
+	 * Executes given callable and returns a Response made from its output
 	 */
-	public function get(string $uri, $callable, array $classArgs = []): self {
-		return $this->route('GET', $uri, $callable, $classArgs);
-	}
+	protected static function captureCallable(callable $callable, array $args): ?Response {
+		ob_start();
+		$result = call_user_func_array($callable, $args);
+		$output = ob_get_clean();
+		$headers = headers_list();
+		$status = http_response_code();
 
-	/**
-	 * Defines a POST route, using route() method
-	 * @param string $uri The URI for the route
-	 * @param mixed $callable A closure or [Controller, action] combination
-	 * @param array $classArgs Optional arguments to the found class' constructor
-	 * @return Router
-	 */
-	public function post(string $uri, $callable, array $classArgs = []): self {
-		return $this->route('POST', $uri, $callable, $classArgs);
-	}
+		if ($result instanceof Response)
+			$response = $result;
+		elseif (is_string($result))
+			$response = new Response($status, $headers, $result);
+		elseif (is_array($result) || is_object($result))
+			$response = new Response($status, array_merge($headers, ['Content-Type: application/json; charset=utf-8']), json_encode($result));
+		elseif ($result !== false)
+			$response = new Response($status, $headers, $output);
+		else
+			$response = null;
 
-	/**
-	 * Defines a PUT route, using route() method
-	 * @param string $uri The URI for the route
-	 * @param mixed $callable A closure or [Controller, action] combination
-	 * @param array $classArgs Optional arguments to the found class' constructor
-	 * @return Router
-	 */
-	public function put(string $uri, $callable, array $classArgs = []): self {
-		return $this->route('PUT', $uri, $callable, $classArgs);
-	}
-
-	/**
-	 * Defines a DELETE route, using route() method
-	 * @param string $uri The URI for the route
-	 * @param mixed $callable A closure or [Controller, action] combination
-	 * @param array $classArgs Optional arguments to the found class' constructor
-	 * @return Router
-	 */
-	public function delete(string $uri, $callable, array $classArgs = []): self {
-		return $this->route('DELETE', $uri, $callable, $classArgs);
-	}
-
-	/**
-	 * Defines a OPTIONS route, using route() method
-	 * @param string $uri The URI for the route
-	 * @param mixed $callable A closure or [Controller, action] combination
-	 * @param array $classArgs Optional arguments to the found class' constructor
-	 * @return Router
-	 */
-	public function options(string $uri, $callable, array $classArgs = []): self {
-		return $this->route('OPTIONS', $uri, $callable, $classArgs);
-	}
-
-	/**
-	 * Defines a GET route, using route() method, that only outputs a template/view
-	 * @param string $uri The URI for the route
-	 * @param string $filename Template name
-	 * @param array $data Optional data for the template
-	 * @return Router
-	 */
-	public function view(string $uri, string $filename, array $data = []): self {
-		return $this->route('GET', $uri, function () use ($filename, $data) {
-			new class ($filename, $data) extends Controller {
-				function __construct($filename, $data) {
-					$this->view($filename, $data);
-				}
-			};
-		});
+		return $response;
 	}
 
 	/**
@@ -289,83 +204,11 @@ class Router {
 	}
 
 	/**
-	 * Dispatches the request to a callable
-	 * @param callable $callable
-	 * @param array $args
-	 * @param Cache|null $cache
-	 */
-	private static function executeCallable(callable $callable, array $args, Cache $cache = null) {
-		$gzip = (strpos($_SERVER['HTTP_ACCEPT_ENCODING'] ?? '', "gzip") !== false && extension_loaded('zlib') ? ".gz" : '');
-
-		$cachename = ($cache && @$_SERVER['REQUEST_METHOD'] == "GET" ? "output".str_replace("/", "-", $_SERVER['REQUEST_URI']).".html$gzip" : false);
-		if ($cachename) {
-			self::setETag(md5($cachename.$cache->getTime($cachename)));
-
-			if (!empty ($_SERVER['HTTP_IF_MODIFIED_SINCE'])) {
-				if ($cache->has($cachename, strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']))) {
-					header_remove();
-					http_response_code(304); // 304 Not Modified
-					exit;
-				}
-			}
-			header("Last-Modified: ".date("r"));
-			header("Vary: Accept-Encoding");
-			if ($cache->has($cachename)) {
-				if ($gzip) {
-					ini_set('zlib.output_compression', 'Off');
-					header('Content-Encoding: gzip');
-				}
-				$cache->dump($cachename);
-				exit;
-			}
-		}
-
-		if ($cachename)
-			ob_start();
-
-		$result = call_user_func_array($callable, $args);
-
-		if (is_string($result))
-			echo $result;
-		elseif (is_array($result) || is_object($result)) {
-			header('Content-Type: application/json; charset=utf-8');
-			echo json_encode($result);
-		}
-
-		if ($cachename)
-			$cache->put($cachename, $gzip ? gzencode(ob_get_contents()) : ob_get_contents());
-
-		if ($result !== false)
-			exit;
-	}
-
-	private static function captureCallable(callable $callable, array $args): ?Response {
-		ob_start();
-		$result = call_user_func_array($callable, $args);
-		$output = ob_get_clean();
-		$headers = headers_list();
-		$status = http_response_code();
-
-		if ($result instanceof Response)
-			$response = $result;
-		elseif (is_string($result))
-			$response = new Response($status, $headers, $result);
-		elseif (is_array($result) || is_object($result))
-			$response = new Response($status, array_merge($headers, ['Content-Type: application/json; charset=utf-8']), json_encode($result));
-		elseif ($result !== false)
-			$response = new Response($status, $headers, $output);
-		else
-			$response = null;
-
-		return $response;
-	}
-
-	/**
 	 * Changes case of the argument as if it was a class name (capital letter on each word boundary)
 	 * @param string $arg The argument
 	 * @return string The result
 	 */
-	private static function classCase(string $arg): string {
+	protected static function classCase(string $arg): string {
 		if (strpbrk($arg, '-.'))
 			return strtr(ucwords($arg, '-.'), ['-' => '', '.' => '']);
 		return ucfirst($arg);
@@ -377,23 +220,10 @@ class Router {
 	 * @param string $arg The argument
 	 * @return string The result
 	 */
-	private static function actionCase(string $method, string $arg): string {
+	protected static function actionCase(string $method, string $arg): string {
 		if (strpbrk($arg, '-.'))
 			return strtr(lcfirst($method.ucwords($arg, '-.')), ['-' => '', '.' => '']);
 		return ($method ? $method.ucfirst($arg) : $arg);
-	}
-
-	/**
-	 * Sets the ETag header and triggers 304 response if ETags match
-	 * @param string $etag The ETag value
-	 */
-	protected static function setETag(string $etag) {
-		if (($before = $_SERVER['HTTP_IF_NONE_MATCH'] ?? ''))
-			if ($before == $etag) {
-				http_response_code(304); // 304 Not Modified
-				exit;
-			}
-		header("ETag: $etag");
 	}
 
 }
--- src/RouterMiddleware.php
+++ src/RouterMiddleware.php
@@ -3,37 +3,54 @@ namespace Subframe;
 
 use Exception;
 
+/**
+ * Routing middleware, defining routes and fitting in the middleware stack
+ */
 class RouterMiddleware implements MiddlewareInterface {
 
-	private $routes = [];
+	/**
+	 * All defined routes
+	 * @var array[]
+	 */
+	protected $routes = [];
 
 	/**
+	 * Processes the request - tries defined routes until a match is found; otherwise throws an Exception
 	 * @throws Exception
 	 */
 	public function process(RequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
 		$router = new Router($request);
-		foreach ($this->routes as [$method, $uri, $action, $args]) {
+		foreach ($this->routes as [$method, $uri, $action, $classArgs]) {
 			if ($method)
-				$response = $router->captureRoute($method, $uri, $action, $args);
+				$response = $router->captureRoute($method, $uri, $action, $classArgs);
 			elseif ($uri !== null)
-				$response = $router->captureViewRoute($uri, $action, $args);
+				$response = $router->captureViewRoute($uri, $action, $classArgs);
 			else
-				$response = $router->captureRouteInNamespace($action, $args);
+				$response = $router->captureRouteInNamespace($action, $classArgs);
 			if ($response)
 				return $response;
 		}
 		throw new Exception('Route not found.', 404);
 	}
 
-	public function addRoute($method, $uri, $callable, $classArgs = []) {
-		$this->routes[] = [$method, $uri, $callable, $classArgs];
+	/**
+	 * Adds a route to the list
+	 */
+	public function addRoute(string $method, string $uri, $action, array $classArgs = []) {
+		$this->routes[] = [$method, $uri, $action, $classArgs];
 	}
 
+	/**
+	 * Adds a view route to the list
+	 */
 	public function addView(string $uri, string $filename, array $data = []) {
 		$this->routes[] = [null, $uri, $filename, $data];
 	}
 
-	public function addNamespace($namespace, $classArgs = []) {
+	/**
+	 * Adds a namespace with routes defined across classes to the list
+	 */
+	public function addNamespace(string $namespace, array $classArgs = []) {
 		$this->routes[] = [null, null, $namespace, $classArgs];
 	}
 
